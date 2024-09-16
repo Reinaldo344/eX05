330b1e2afb2c4893576fbd7af9287ee9ba70277b
Reinaldo344 <161724821+Reinaldo344@users.noreply.github.com>
2024-09-16 09:08:17 -0300

    Create pong.py

---
+++ pong.py
@@ -0,0 +1,19 @@
+
+import pygame
+import sys 
+
+# inicializa o pygama
+pygame.init()
+
+# dimensões da tela 
+largura = 800
+altura = 400
+tela = pygame.desplay.set_medo((largura, altura))
+pygame.display.set_cation("pong")
+
+#cores
+branco = (255, 255, 255)
+preto = (0, 0, 0)
+alul = (0, 128, 255)
+verde = (0, 255, 0)
+amarelo = (255, 255, 0)
